2e019da297397b70ba331375b55530cac68627b0
SypherRed <ext.sypher@gmail.com>
2026-07-11 23:46:56 +0200

    Add melee dodge mechanic for Hunterphage gene and related patch

--- Source/DarkEssence/DarkEssenceDefof.cs
+++ Source/DarkEssence/DarkEssenceDefof.cs
@@ -8,6 +8,8 @@ public static class DarkEssenceDefof
 {
     [MayRequireBiotech] public static BodyPartDef Heart;
 
+    [MayRequireBiotech] public static GeneDef Gene_Hunterphage_MeleeGifted;
+
     static DarkEssenceDefof()
     {
         DefOfHelper.EnsureInitializedInCtor(typeof(DarkEssenceDefof));
